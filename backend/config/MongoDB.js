7d85d36967e3219b6d42a64d84d27bd9b100591e
Victor Nathanael <victornathanael26@gmail.com>
2022-06-15 14:49:50 +0700

    fixed commit

--- backend/config/MongoDB.js
+++ backend/config/MongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URL, {
+        const connection = mongoose.connect(process.env.MONGO_URL, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         });
