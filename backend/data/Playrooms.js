ea084bda3cce7c155f70f011c5e37b78c76064ed
Victor Nathanael <victornathanael26@gmail.com>
2022-06-20 00:11:07 +0700

    Update Playrooms.js

--- backend/data/Playrooms.js
+++ backend/data/Playrooms.js
@@ -1,26 +1,93 @@
 
 const playrooms = [];
 
-let tableCount = 0;
-for (let index = 1; index <= 20; index++) {
-    let minigame
-    if (index >= 1 && index <= 5) {
-        minigame = "Congklak"
-    } else if (index >= 6 && index <= 10) {
-        minigame = "Gobak Sodor"
-    } else if (index >= 11 && index <= 15) {
-        minigame = "Tarik Tambang"
-    } else {
-        minigame = "Balap Karung"
-    }
-    let tempArray = [];
-    for (let j = 1; j <= 4; j++) {
-        tempArray.push(++tableCount)
-    }
+for (let index = 1; index <= 5; index++) {
     let temp = {
         playroom_id: index,
-        game_name: minigame,
-        table_ids: tempArray,
+        x: 10,
+        y: 10,
+        room_name: "Test Congklak Room",
+        floor: "ground_test",
+        wall: "wall_test",
+        spawn: {
+            spawn_x: 137.5,
+            spawn_y: 25,
+            spawn_z: 12.5,
+        },
+        objects: [
+            {
+                mesh: "desk",
+                URL: "",
+                position_x: 4,
+                position_y: 0,
+                position_z: 4,
+                size_x: 2,
+                size_z: 2
+            },
+            {
+                mesh: "desk",
+                URL: "",
+                position_x: 8,
+                position_y: 0,
+                position_z: 4,
+                size_x: 2,
+                size_z: 2
+            },
+            {
+                mesh: "desk",
+                URL: "",
+                position_x: 4,
+                position_y: 0,
+                position_z: 8,
+                size_x: 2,
+                size_z: 2
+            },
+            {
+                mesh: "desk",
+                URL: "",
+                position_x: 8,
+                position_y: 0,
+                position_z: 8,
+                size_x: 2,
+                size_z: 2
+            },
+            {
+                mesh: "congklak",
+                URL: "./pages/games/congklak_games.html",
+                position_x: 8,
+                position_y: 25,
+                position_z: 8,
+                size_x: 1,
+                size_z: 1
+            },
+            {
+                mesh: "congklak",
+                URL: "./pages/games/congklak_games.html",
+                position_x: 4,
+                position_y: 25,
+                position_z: 4,
+                size_x: 1,
+                size_z: 1
+            },
+            {
+                mesh: "congklak",
+                URL: "./pages/games/congklak_games.html",
+                position_x: 8,
+                position_y: 25,
+                position_z: 4,
+                size_x: 1,
+                size_z: 1
+            },
+            {
+                mesh: "congklak",
+                URL: "./pages/games/congklak_games.html",
+                position_x: 4,
+                position_y: 25,
+                position_z: 8,
+                size_x: 1,
+                size_z: 1
+            }
+        ],
     }
     playrooms.push(temp)
 
